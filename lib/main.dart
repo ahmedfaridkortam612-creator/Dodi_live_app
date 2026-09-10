b0f9b78d582898e504928636bda98157ca2497bc
ahmedfaridkortam612-creator <ahmedfaridkortam612@gmail.com>
2026-09-10 09:56:54 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,23 @@
 import 'package:flutter/material.dart';
-import 'auth_screen.dart'; // استيراد صفحة التسجيل والبروفايل
-import 'daily_payout_screen.dart';
-import 'recharge_modal.dart';
-import 'services/wallet_api_service.dart'; // تأكد من مسار الخدمة حسب مجلداتك
+import 'auth_screen.dart';
 
 void main() {
   runApp(const DodiLiveApp());
 }
 
 class DodiLiveApp extends StatelessWidget {
-  const DodiLiveApp({super.key});
+  const DodiLiveApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dodi Live App',
+      title: 'Dodi Live',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        brightness: Brightness.dark,
         primarySwatch: Colors.pink,
-        scaffoldBackgroundColor: Colors.black,
-        useMaterial3: true,
       ),
-      // جعل شاشة تسجيل الدخول هي الواجهة الأولى للتطبيق
-      home: AuthScreen(
-        onLoginSuccess: () {
-          // دالة الانتقال للمحفظة بعد النجاح (يمكنك تعديلها لاحقاً)
-        },
-      ),
+      home: const AuthScreen(),
     );
   }
 }
