5175e4468456439c3aa7fccc1f6cd091857cb555
ahmedfaridkortam612-creator <ahmedfaridkortam612@gmail.com>
2026-09-13 17:56:48 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'screens/auth_screen.dart';
 
-void main() {
+void main() async {
+  // خطوة إجبارية عشان فلاتر يجهز نفسه قبل تشغيل السيرفر
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // تشغيل وتهيئة اتصال السيرفر بـ Firebase
+  await Firebase.initializeApp();
+  
   runApp(const DodiLiveApp());
 }
 
@@ -16,7 +23,7 @@ class DodiLiveApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.dark,
         primarySwatch: Colors.purple,
-        scaffoldBackgroundColor: const Color(0xFF000000),
+        scaffoldBackgroundColor: const Color(0xFF0B0716),
       ),
       home: const AuthScreen(),
     );
